49895752f04abd9849810b9819ebcd9a0ca7842f
Fialho <ffialho266@gmail.com>
2026-09-17 01:02:05 -0300

    Listagem de produtos pt-1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 
+      gap-x-4 gap-y-8 justify-items-center">
+
+      </div>
       <h1>Page-commerce</h1>
     </div>
   );
